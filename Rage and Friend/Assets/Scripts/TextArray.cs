6d2561abda07dc583c3933a45a7eaca91beb73e7
RamzCas <2749414@students.wits.ac.za>
2026-01-07 14:42:22 +0200

    script thing done

--- Rage and Friend/Assets/Scripts/TextArray.cs
+++ Rage and Friend/Assets/Scripts/TextArray.cs
@@ -1,6 +1,15 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextArray : MonoBehaviour
 {
     public string[] Questions;
+    public TextMeshProUGUI TextMeshProUGUI;
+
+    private void Update()
+    {
+        TextMeshProUGUI.text = Questions[0];
+    }
 }
