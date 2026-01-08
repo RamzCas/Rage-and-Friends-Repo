6469fcc106fe303afc79546f0a24a13a4263d820
RamzCas <2749414@students.wits.ac.za>
2026-01-08 20:13:45 +0200

    work on lvl 3

--- Rage and Friend/Assets/Scripts/Wind.cs
+++ Rage and Friend/Assets/Scripts/Wind.cs
@@ -5,12 +5,24 @@ public class Wind : MonoBehaviour
 {
     public string Tag;
     public GameObject VolumeBlur;
+    private GameObject Player;
+    private CharacterController characterController;
+
+
+    private void Awake()
+    {
+        Player = GameObject.FindWithTag("Player");
+        characterController = Player.GetComponent<CharacterController>();
+    }
+
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag(Tag))
         {
           VolumeBlur.SetActive(true);
+            Vector3 move = Player.transform.up * 50f * Time.deltaTime;
+            characterController.Move(move);
         }
     }
 
