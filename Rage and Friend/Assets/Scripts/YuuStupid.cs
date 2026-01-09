a6b95e844c052bbcaf909d9d1838332aa0970fa7
RamzCas <2749414@students.wits.ac.za>
2026-01-09 17:12:00 +0200

    wrong mech

--- Rage and Friend/Assets/Scripts/YuuStupid.cs
+++ Rage and Friend/Assets/Scripts/YuuStupid.cs
@@ -1,12 +1,35 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class YuuStupid : MonoBehaviour
 {
+    private GameObject Player;
+    private CharacterControler CharacterControler;
+    public GameObject Stupid;
+    public GameObject RickRoll;
+    private void Awake()
+    {
+        Player = GameObject.FindWithTag("Player");
+        CharacterControler = Player.GetComponent<CharacterControler>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("killed");
+            StartCoroutine(KillPlayer());
         }
+    }
+
+    public IEnumerator KillPlayer() 
+    {
+        CharacterControler.enabled = false;
+        Stupid.SetActive(true);
+        yield return new WaitForSeconds(5);
+        RickRoll.SetActive(true);
+        yield return new WaitForSeconds(3.5f);
+        Destroy(GameObject.FindWithTag("GM"));
+        SceneManager.LoadSceneAsync(2);
     }
 }
