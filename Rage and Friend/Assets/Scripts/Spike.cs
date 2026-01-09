5ec39496ca17637a8e1e453c59f23b1910c620c0
RamzCas <2749414@students.wits.ac.za>
2026-01-09 02:54:06 +0200

    RickRoll working

--- Rage and Friend/Assets/Scripts/Spike.cs
+++ Rage and Friend/Assets/Scripts/Spike.cs
@@ -1,17 +1,28 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Spike : MonoBehaviour
 {
     private GameObject Rick;
+    public GameObject RickRoll;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) 
         {
             Debug.Log("killed");
             //Rick = GameObject.FindWithTag("Rick");
-            SceneManager.LoadSceneAsync(2);
-            Destroy(GameObject.FindWithTag("GM"));
+            //SceneManager.LoadSceneAsync(2);
+            //Destroy(GameObject.FindWithTag("GM"));
+            StartCoroutine(KillPlayer());
         }
+    }
+
+    public IEnumerator KillPlayer() 
+    {
+        RickRoll.SetActive(true);
+        yield return new WaitForSeconds(4);
+        Destroy(GameObject.FindWithTag("GM"));
+        SceneManager.LoadSceneAsync(2);
     }
 }
