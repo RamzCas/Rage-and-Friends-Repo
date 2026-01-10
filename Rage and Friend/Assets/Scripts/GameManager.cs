efa532c1f31ebf4493d812a9cbf210bf48f8b89d
RamzCas <2749414@students.wits.ac.za>
2026-01-10 14:22:01 +0200

    !!!!!!!!!!

--- Rage and Friend/Assets/Scripts/GameManager.cs
+++ Rage and Friend/Assets/Scripts/GameManager.cs
@@ -1,16 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
-   
+    public GameObject GameObject;
+    public List<GameObject> GameNames = new List<GameObject>();
     private void Awake()
     {
        DontDestroyOnLoad(this.gameObject);
+        
+    }
+
+    private void Update()
+    {
+      
     }
 
 
+
+    public void create() 
+    {
+     Instantiate(gameObject, new Vector3(0,0,0), Quaternion.identity);
+    }
 }
