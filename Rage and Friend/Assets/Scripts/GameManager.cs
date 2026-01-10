2fd479a36d6d42c684514b362cd175b45e1c5781
RamzCas <2749414@students.wits.ac.za>
2026-01-10 13:34:45 +0200

    work on scenes

--- Rage and Friend/Assets/Scripts/GameManager.cs
+++ Rage and Friend/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
        DontDestroyOnLoad(this.gameObject);
-        
     }
+
+
 }
