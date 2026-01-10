faa22502462794b2029bf52a0b085e4e056f7982
RamzCas <2749414@students.wits.ac.za>
2026-01-10 12:13:06 +0200

    lvl 5 done

--- Rage and Friend/Assets/Scripts/MoveKey.cs
+++ Rage and Friend/Assets/Scripts/MoveKey.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class MoveKey : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int move;
+    public bool OpenDoor;
 
-    // Update is called once per frame
+    public GameObject[] Tps;
+    public GameObject Key;
+    public GameObject Wall;
+
+    public AudioClip Yeet;
+    public AudioClip Correct;
+    public AudioSource AudioSource;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") & OpenDoor) 
+        {
+            Destroy(Wall);
+            AudioSource.PlayOneShot(Correct);
+            Destroy(Key);
+        }
+
+        if (other.gameObject.CompareTag("Player") & !OpenDoor)
+        {
+            move++;
+            AudioSource.PlayOneShot(Yeet);
+        }
+    }
     void Update()
     {
-        
+        if (move == 1) 
+        {
+            Key.transform.position = Tps[0].transform.position;
+          
+        }
+
+        if (move == 2)
+        {
+            Key.transform.position = Tps[1].transform.position;
+            
+        }
+
+        if (move == 3)
+        {
+            Key.transform.position = Tps[2].transform.position;
+            
+        }
+
+        if (move == 4)
+        {
+            OpenDoor = true;
+         
+        }
     }
 }
