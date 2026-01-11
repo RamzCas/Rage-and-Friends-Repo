943c1f521d3fba557fe6b9c570fcc57c02babb66
RamzCas <2749414@students.wits.ac.za>
2026-01-11 10:45:24 +0200

    in process

--- Rage and Friend/Assets/Scripts/GameManager.cs
+++ Rage and Friend/Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     //public GameObject[] ChosenLvlsStage;
     public List<GameObject> ChosenLvls;
     public LvlBuilder[] lvlBuilders;
-    public List<GameObject> SpawnPoints;
+    public List<Transform> SpawnPoints;
 
     [Header("Game Management")]
     public int LevelsCompleted;
@@ -31,7 +31,7 @@ public class GameManager : MonoBehaviour
     {
         if (GameInPlay) 
         {
-            if (LevelsCompleted == 0)
+            if (LevelsCompleted <= 1)
             {
                 ChosenLvls[0].SetActive(true);
                 ChosenLvls[1].SetActive(false);
@@ -57,7 +57,54 @@ public class GameManager : MonoBehaviour
             ChosenLvls.Add(Lvl1);
         }
 
-        
+        if (lvlBuilders[0].Lvl2Selected)
+        {
+            Instantiate(Lvl2, SpawnPoints[0].transform.parent);
+            ChosenLvls.Add(Lvl2);
+        }
+
+        if (lvlBuilders[0].Lvl3Selected)
+        {
+            Instantiate(Lvl3, SpawnPoints[0].transform.parent);
+            ChosenLvls.Add(Lvl3);
+        }
+
+        if (lvlBuilders[0].Lvl4Selected)
+        {
+            Instantiate(Lvl4, SpawnPoints[0].transform.parent);
+            ChosenLvls.Add(Lvl4);
+        }
+
+        if (lvlBuilders[0].Lvl5Selected)
+        {
+            Instantiate(Lvl5, SpawnPoints[0].transform.parent);
+            ChosenLvls.Add(Lvl5);
+        }
+
+        if (lvlBuilders[0].Lvl6Selected)
+        {
+            Instantiate(Lvl6, SpawnPoints[0].transform.parent);
+            ChosenLvls.Add(Lvl6);
+        }
+
+        //==========Lvl2=================
+
+        if (lvlBuilders[1].Lvl1Selected)
+        {
+            Instantiate(Lvl1, SpawnPoints[1].transform.parent);
+            ChosenLvls.Add(Lvl1);
+        }
+
+        if (lvlBuilders[1].Lvl2Selected)
+        {
+            Instantiate(Lvl2, SpawnPoints[1].transform.parent);
+            ChosenLvls.Add(Lvl2);
+        }
+        if (lvlBuilders[1].Lvl3Selected)
+        {
+            Instantiate(Lvl3, SpawnPoints[1].transform.parent);
+            ChosenLvls.Add(Lvl3);
+        }
     }
 
 }
